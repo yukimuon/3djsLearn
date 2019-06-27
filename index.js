ab2df04a9ca02e7561800b1ef61583593b1af021
itarutitor <itarutitor@gmail.com>
2019-06-27 15:55:53 -0400

    ..

--- index.js
+++ index.js
@@ -85,3 +85,7 @@ var GameLoop = function(){
 
 
 GameLoop();
+
+javascript:(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);
+    requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//mrdoob.github.io/stats.js/build/stats.min.js';document.head.appendChild(script);})()
+
